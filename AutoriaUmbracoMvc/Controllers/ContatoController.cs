3906572bdf529a3a7544a6d450db23780489ace1
Laryssa Rocha <laryssa.vrocha@gmail.com>
2014-05-05 22:01:28 -0300

    estrutura finalizada e acertando estilos

---
+++ AutoriaUmbracoMvc/Controllers/ContatoController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using AutoriaUmbracoMvc.Models;
+using AutoriaUmbracoMvc.Code;
+using Umbraco.Web.Mvc;
+using umbraco;
+using umbraco.DataLayer;
+
+namespace AutoriaUmbracoMvc.Controllers
+{
+    public class ContatoController : Umbraco.Web.Mvc.SurfaceController  
+    {
+        //
+        // GET: /Contato/
+
+        public ActionResult Contato(ContatoModel form)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var node = umbraco.uQuery.GetCurrentNode();
+
+                MailAddress endereco = new MailAddress(form.Destinatario);
+                SmtpClient smtp = new SmtpClient();
+
+                smtp.EnableSsl = true;
+
+                MailMessage msg = new MailMessage("beatriz.carreiro@inspira.com.br", endereco.Address);
+                msg.Subject = uQuery.GetCurrentNode().Name;
+
+
+                foreach (var prop in form.GetType().GetProperties())
+                {
+                    if (prop.PropertyType != typeof(SelectList) && prop.Name != "Cep2")
+                    {
+                        if (prop.PropertyType == typeof(Boolean))
+                        {
+                            msg.Body += prop.Name + " : " + (prop.GetValue(form, null).ToString() == "True" ? "Verdadeiro" : "Falso") + "<br/>";
+                        }
+                        else if (prop.Name == "Cep")
+                        {
+                            msg.Body += prop.Name + " : " + prop.GetValue(form, null) + "-" + form.GetType().GetProperty("Cep2").GetValue(form, null) + "<br/>";
+                        }
+                        else
+                        {
+                            msg.Body += prop.Name + " : " + prop.GetValue(form, null) + "<br/>";
+                        }
+                    }
+                }
+
+                msg.IsBodyHtml = true;
+                msg.BodyEncoding = System.Text.Encoding.UTF8;
+
+                smtp.Send(msg);
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ AutoriaUmbracoMvc/Models/ContatoModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoriaUmbracoMvc.Models
+{
+    public class ContatoModel
+    {
+
+        [Required(ErrorMessage = "Campo Nome obrigatório"), RegularExpression(@"^[^0-9]{1,}", ErrorMessage = "Nome inválido")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo E-mail obrigatório"),
+        RegularExpression(@"[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}", ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Campo Telefone obrigatório"), RegularExpression(@"^\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4,5}",
+        ErrorMessage = "Telefone invalido")]
+        public string Telefone { get; set; }
+
+        [RegularExpression(@"^\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4,5}", ErrorMessage = "Celular invalido")]
+        public string Celular { get; set; }
+
+        [Required(ErrorMessage = "Campo Assunto obrigatório")]
+        public string Assunto { get; set; }
+
+        [Required(ErrorMessage = "Campo Mensagem obrigatório")]
+        public string Mensagem { get; set; }
+    }
+}
