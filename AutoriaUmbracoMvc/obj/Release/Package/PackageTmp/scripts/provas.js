0dd939ac3c46b5c13551b60402b96c1d6f1090d6
Laryssa Rocha <laryssa.vrocha@gmail.com>
2014-05-11 23:44:05 -0300

    obj

---
+++ AutoriaUmbracoMvc/obj/Release/Package/PackageTmp/scripts/provas.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    $('.tabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+    $(".tabs a:first").click();
+
+});
